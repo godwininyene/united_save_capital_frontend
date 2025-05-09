ed4c639bd2759d633fdf07ba77501ed1feeb2258
godwininyene <godwinhigh2@gmail.com>
2025-05-09 06:56:01 -0700

    WIP: Added chat widget

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -9,7 +9,22 @@ const BaseLayout = ()=>{
 
     useEffect(()=>{
         AOS.init()
+        buildTawkChatWidget()
     },[])
+
+     
+    
+    
+    const buildTawkChatWidget = async()=>{
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/681e086f08bed819150daffb/1iqql1tpo';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    
+    }
     return(
         <div className="bg-primary w-full overflow-hidden">
             <div className={`${styles.boxWidth} relative z-[9999]`}>
